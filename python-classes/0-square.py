ab9040452de12f3a22cd31ad8823b0412537216a
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-23 17:48:40 +0000

    This is a commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""Module that defines a Square class"""
+
+
 class Square:
+    """A class that defines a square by its size"""
+    
     def __init__(self, size):
+        """Initialize a square with a given size
+        
+        Args:
+            size: The size of the square (no type/value verification)
+        """
         self.__size = size
