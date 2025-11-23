31d9fac4dfac3f38831ef954c6234c750cb49634
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-23 18:00:22 +0000

    This is a commit

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module that defines a Square class"""
+"""Module that defines a Square class with size getter/setter"""
 
 
 class Square:
@@ -7,12 +7,22 @@ class Square:
 
     def __init__(self, size=0):
         """Initialize square"""
-        if type(size) is not int:
+        self.size = size   # IMPORTANT: use setter for validation
+
+    @property
+    def size(self):
+        """Get current size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set size with validation"""
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     def area(self):
-        """Returns the area of the square"""
+        """Return area"""
         return self.__size * self.__size
